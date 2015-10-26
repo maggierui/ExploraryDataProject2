be14b4ee23f5f1fd9d148822fa07995b7d684c5c
Rui Hu <maggiehu.china@gmail.com>
2015-10-26 12:03:06 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -11,3 +11,10 @@ library(ggplot2)
 qplot(year,Emissions,data=NEI_sum_Baltimore,geom=c("point","smooth"),method="lm", facets = type~.)+ggtitle("Baltimore Emissions by Types")+scale_x_continuous(name="Year",breaks=c(1999,2002,2005,2008))
 
 ggsave("plot3.png")
+
+##another way to plot:
+## ggplot(baltimore,aes(factor(year), Emissions, fill = type)) +
+          geom_bar(stat="identity") + theme_light() + guides(fill=FALSE)+ 
+          facet_grid(.~type, scales = "free", space="free") + 
+          labs(x="Year", y=expression("Total PM2.5 Emissions (Tons)")) + 
+          labs(title = expression( "Annual PM2.5 Emissions for Baltimore by Source Type (1999 to 2008)"))
