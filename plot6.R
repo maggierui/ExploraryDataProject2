877125aa47f5cc42b6375d0b61f504ca814e2da2
Rui Hu <maggiehu.china@gmail.com>
2015-10-26 12:06:16 -0400

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -14,3 +14,14 @@ library(ggplot2)
 p<-ggplot(subset(NEI_sum_mobile,fips %in% c("24510","06037")))+geom_line(aes(year, Emissions, group=fips, colour=fips))+ggtitle("Emissions by Baltimore and LA")+scale_x_continuous(name="Year",breaks=c(1999,2002,2005,2008))+theme(legend.position="top",plot.title = element_text(size = rel(0.7)))
 p+scale_color_hue(name="Location:",labels=c("Los Angeles","Baltimore"))
 ggsave("plot6.png")
+
+##another way to plot - more visually appealing
+## ggplot(bal_LA, aes(x=factor(year), y=Emissions, fill=city)) +
+          geom_bar(aes(fill=year),stat="identity") + 
+          facet_grid(scales="free", space="free", .~city) +
+          guides(fill=FALSE) + theme_gray() +
+          labs(x="year", y=expression("Total PM2.5 Emissions (Tons)")) + 
+          labs(ggtitle(expression(atop(" PM2.5 emission changes", 
+                      atop(italic("Baltimore Vs. Los Angeles (1999-2008)"), "")))))     
+     
+     dev.off()
